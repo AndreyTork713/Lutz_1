5ced2daebd6c7c80f3c1489dee68b53d8d8c9991
Andrey <tork713@gmail.com>
2019-05-08 07:01:59 +0700

    Добавьте файлы проекта.

---
+++ Lutz_1/Lutz_1.py
@@ -0,0 +1,9 @@
+bob = {'name':'Bob Smith', 'age':42, 'pay':30000, 'job':'software'}
+sue = {'name':'Sue Jonson', 'age':40, 'pay':40000, 'job':'software'}
+
+print(bob['name'],'\n', sue['name'])
+
+print(bob['name'].split()[-1])
+
+sue['pay'] *= 1.25
+print(sue['pay'])
